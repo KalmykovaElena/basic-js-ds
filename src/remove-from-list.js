9a9b3dc58003f82c366c3465dbb187797c121df0
KalmykovaElena <kalmykhovae@gmail.com>
2022-10-17 09:56:07 +0300

    remove

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -23,7 +23,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 function removeKFromList(l, k) {
- 
+  if(l == null){
+    return l;
+}
+while(l.value == k){
+    l = l.next;
+}
+thisNode = l;
+nextNode = thisNode.next;
+while(nextNode != null){
+    if(nextNode.value == k){
+      if(nextNode.next&&nextNode.next.value==k&&nextNode.next.next!==null){
+        thisNode.next = nextNode.next.next
+      }else{
+        thisNode.next = nextNode.next;
+      }
+    
+     
+        if(thisNode.next == null)
+            break;
+    }
+    thisNode = thisNode.next;
+    nextNode = thisNode.next;       
+}
+return l;
 }
 
 module.exports = {
